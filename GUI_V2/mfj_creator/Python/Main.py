ded6804b6e93f041ddb8da582b7cff4b07db3141
jackaranda <50349913+jackarandatree@users.noreply.github.com>
2023-10-03 15:48:09 +1100

    Hacky fix unix issues

--- GUI_V2/mfj_creator/Python/Main.py
+++ GUI_V2/mfj_creator/Python/Main.py
@@ -11,15 +11,15 @@ def run(directory,csv,sdf2xyz2sdf_Directory,charge,parameters):
 	
 	if csv != '':
 		opf = open(csv,'r')
-		logs = opf.read().split('\n')
+		logs = opf.read().split('/n')
 		opf.close()
 		logs = [x for x in logs if x.lower().endswith('.log') or x.lower().endswith('.out') and '_atom' not in x]
 	else:
 		logs = [x for x in os.listdir(directory) if x.lower().endswith('.log') or x.lower().endswith('.out') and '_atom' not in x]
 	print('Process estimated to take '+str(round((len(logs)*2.5+10)/(60*2),2))+' minutes.')
 	
-	if directory[-1] != '\\':
-		directory = directory+'\\'
+	if directory[-1] != '//':
+		directory = directory+'//'
 	try:
 		os.mkdir(directory+'Mobcal_Inputs')
 	except:
@@ -49,25 +49,25 @@ def run(directory,csv,sdf2xyz2sdf_Directory,charge,parameters):
 	#Copy files so that the originals don't get changed	
 	for file in logs:
 		try:
-			copyfile(directory+file,directory+'Mobcal_Inputs\\'+file)
+			copyfile(directory+file,directory+'Mobcal_Inputs//'+file)
 		except (PermissionError):
 			pass
 	directory = directory+'Mobcal_Inputs' #Rename global directory variable to new directory
 
 	#Other error handling
 
-	#Check if Babel is on PC
-	App_Data = os.listdir(os.getenv('APPDATA'))
-	Babel_Installed = [x for x in App_Data if x == 'OpenBabel-2.4.1']
-	if not Babel_Installed:
-		msg = QMessageBox()
-		msg.setWindowTitle('Software Error')
-		msg.setText('Open Babel could not be found on your pc, are you running the version provided?')
-		msg.setIcon(QMessageBox.Critical)
-		msg.exec_() #show messagebox
-		return
+	#Check if Babel is on PC - Does not work for Unix
+	#App_Data = os.listdir(os.getenv('APPDATA'))
+	#Babel_Installed = [x for x in App_Data if x == 'OpenBabel-2.4.1']
+	#if not Babel_Installed:
+	#	msg = QMessageBox()
+	#	msg.setWindowTitle('Software Error')
+	#	msg.setText('Open Babel could not be found on your pc, are you running the version provided?')
+	#	msg.setIcon(QMessageBox.Critical)
+	#	msg.exec_() #show messagebox
+	#	return
 	
-	if not os.path.isdir(os.getcwd()+'\mfj_creator\\Python'):
+	if not os.path.isdir(os.getcwd()+'/mfj_creator//Python'):
 		msg = QMessageBox()
 		msg.setWindowTitle('Software Error')
 		msg.setText('The required python files are missing. Please redownload from the GitHub Repo and do not remove anything.')
@@ -75,13 +75,13 @@ def run(directory,csv,sdf2xyz2sdf_Directory,charge,parameters):
 		msg.exec_() #show messagebox
 		return
 	
-	directory = directory.replace('//','\\') #Make sure it is in command prompt lingo
-	if directory[-1] != '\\': #If the last character is not a \ add a slash
-		directory = directory+'\\'
+	directory = directory.replace('//','//') #Make sure it is in command prompt lingo
+	if directory[-1] != '//': #If the last character is not a / add a slash
+		directory = directory+'//'
 	
 	print('If any errors are encountered they will be written to: '+directory+'Errors.csv')
-	if os.path.isfile(directory+'\Errors.csv'): #Delete old error files before each run
-		os.remove(directory+'\Errors.csv')
+	if os.path.isfile(directory+'/Errors.csv'): #Delete old error files before each run
+		os.remove(directory+'/Errors.csv')
 
 	##### -- ESP Data -- #####
 	ESP = []
@@ -89,11 +89,11 @@ def run(directory,csv,sdf2xyz2sdf_Directory,charge,parameters):
 	
 	#Extracting ESP data from Gaussian .log files
 	if check == [1,0]:
-		print('Extracting ESP info from Gaussian logs.\n')
+		print('Extracting ESP info from Gaussian logs./n')
 
 		for file in logs:
 			opf = open(directory+file,'r')
-			data = opf.read().replace('\n','ggez')
+			data = opf.read().replace('/n','ggez')
 			opf.close()
 
 			#Get ESP charge data from Gaussian files
@@ -113,11 +113,11 @@ def run(directory,csv,sdf2xyz2sdf_Directory,charge,parameters):
 	
 	#Extracting ESP data from ORCA .out files
 	if check == [0,1]:
-		print('Extracting ESP info from ORCA .out files.\n')
+		print('Extracting ESP info from ORCA .out files./n')
 
 		for file in logs:
 			opf = open(directory+file,'r')
-			data = opf.read().replace('\n','ggez')
+			data = opf.read().replace('/n','ggez')
 			opf.close()		
 
 		#Get ESP charge data from ORCA out files
@@ -137,16 +137,16 @@ def run(directory,csv,sdf2xyz2sdf_Directory,charge,parameters):
 	#There is a problem when converting .out files to .sdf using BABEL.... So we're going to make a .xyz file from the ORCA .out because that conversion works just fine 
 	outs = [x for x in os.listdir(directory) if x.lower().endswith('.out')]
 	for filename in outs:
-		opf = open(directory+'\\'+filename,'r')
+		opf = open(directory+'//'+filename,'r')
 		data = opf.read()
 		opf.close()
 
 		#Get the geometry and write to list called geoemtry
 		geometry=[] #Get geometry of atoms from file
-		GEOM = re.findall(r'CARTESIAN COORDINATES \(ANGSTROEM\)([\s\S]*?)CARTESIAN COORDINATES \(A.U.\)',data)
+		GEOM = re.findall(r'CARTESIAN COORDINATES /(ANGSTROEM/)([/s/S]*?)CARTESIAN COORDINATES /(A.U./)',data)
 
 		#Loop through each line in the geom block, split each line, then append to geometry list
-		for i in GEOM[-1].split('\n')[2:-3]:
+		for i in GEOM[-1].split('/n')[2:-3]:
 			i = i.split()
 			if len(i) > 0:
 				geometry.append(i)
@@ -154,14 +154,14 @@ def run(directory,csv,sdf2xyz2sdf_Directory,charge,parameters):
 		#Write the geometry to a formatted string
 		fs = ''
 		for i in geometry:
-			fs+='%2s    %12s    %12s    %12s\n'%(i[0],i[1],i[2],i[3])    
+			fs+='%2s    %12s    %12s    %12s/n'%(i[0],i[1],i[2],i[3])    
 
 		#At long last, we can write all the info to the .xyz file
-		opf = open(directory+'\\'+filename[:-4]+'.xyz','w')
-		opf.write(str(len(geometry))+'\n')
-		opf.write(str(filename[:-4])+'\n') #Write filename without extension as the title in the xyz file (Needed for conversion to sdf)
+		opf = open(directory+'//'+filename[:-4]+'.xyz','w')
+		opf.write(str(len(geometry))+'/n')
+		opf.write(str(filename[:-4])+'/n') #Write filename without extension as the title in the xyz file (Needed for conversion to sdf)
 		opf.write(fs)
-		opf.write('\n')
+		opf.write('/n')
 		opf.close()
 
 	##### -- Babel -- #####
@@ -169,7 +169,7 @@ def run(directory,csv,sdf2xyz2sdf_Directory,charge,parameters):
 	# For handling Gaussian log files
 	if check == [1,0]:
 	
-		print('Converting logs to sdf.\n')
+		print('Converting logs to sdf./n')
 
 		babel_i = directory+'*.log'
 		babel_o = directory+'*.sdf'
@@ -193,7 +193,7 @@ def run(directory,csv,sdf2xyz2sdf_Directory,charge,parameters):
 	# For handling ORCA .xyz files (BABEL doesn't seem to like converting the .out file to a .xyz)
 	if check == [0,1]:
 	
-		print('Converting ORCA xyz to sdf.\n')
+		print('Converting ORCA xyz to sdf./n')
 
 		babel_i = directory+'*.xyz'
 		babel_o = directory+'*.sdf'
@@ -222,8 +222,8 @@ def run(directory,csv,sdf2xyz2sdf_Directory,charge,parameters):
 		data = opf.readlines()
 		opf.close()
 
-		if data[0].find('\\') != -1:
-			data[0] = (data[0][data[0].rfind('\\')+1:-5]+'\n') #Fix filename
+		if data[0].find('//') != -1:
+			data[0] = (data[0][data[0].rfind('//')+1:-5]+'/n') #Fix filename
 
 		#print('Converting '+data[0][:-1]+'.sdf to .xyz and .key files')
 
@@ -259,7 +259,7 @@ def run(directory,csv,sdf2xyz2sdf_Directory,charge,parameters):
 				if key_data[index].split()[0] == 'charge':
 					key_data[index] = (key_data[index].split())
 					key_data[index][2] = ESP[file_num][index]
-					key.write('%s %s %s\n'%(key_data[index][0],key_data[index][1],key_data[index][2]))
+					key.write('%s %s %s/n'%(key_data[index][0],key_data[index][1],key_data[index][2]))
 				else:
 					key.write(key_data[index])
 			key.close()
@@ -271,14 +271,14 @@ def run(directory,csv,sdf2xyz2sdf_Directory,charge,parameters):
 			msg.exec_() #show messagebox	
 			return		
 	 
-		#print('Creating '+file[:-4]+'.mfj\n')
+		#print('Creating '+file[:-4]+'.mfj/n')
 
 		key_file = babel_o[:-5]+data[0][:-1]+'.key'
 		xyz_file = babel_o[:-5]+data[0][:-1]+'.xyz'
 		mfj_file = babel_o[:-5]+data[0][:-1]+'.mfj'
 
 		#Pass files to xyz_to_mfj python code
-		xyz_to_mfj(os.getcwd()+r'\mfj_creator\\Python\\',xyz_file,key_file,mfj_file,charge,parameters)
+		xyz_to_mfj(os.getcwd()+r'/mfj_creator//Python//',xyz_file,key_file,mfj_file,charge,parameters)
 
 		#Delete old files
 		os.remove(babel_o[:-5]+file) #sdf file
